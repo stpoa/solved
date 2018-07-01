cce8127006d77002ba5b33d213f83dd512da0529
Tomasz Klęk <admin@Tomaszs-MacBook-Pro.local>
2018-07-01 22:30:53 +0200

    Set tslint rules
    bind Home component and add some rules in tsconfig
    add tslint with rules
    add more rules

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,13 +1,15 @@
-import React from 'react';
-import { render } from 'react-dom';
+import * as React from 'react'
+import { render } from 'react-dom'
 
-const Root = () => {
-    return <div>"Hello World!"</div>;
-};
+import Home from './pages/Home/Home'
+
+const Root = () =>
+    <div>
+        <div>Header</div>
+        <Home />
+        <div>Footer</div>
+    </div>
 
 // main
-const root = document.getElementById("root");
-
-
+const root = document.getElementById('root')
 render(<Root />, root)
-
---
+++ client/src/pages/Home/Home.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react'
+import { Component } from 'react'
+
+export default class Home extends Component {
+
+  public render () {
+    return <div>Home Component!</div>
+  }
+
+}
