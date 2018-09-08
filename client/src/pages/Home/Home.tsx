72ab706183201f43cd6da491dd85e8e703b43528
morganfree <sebastiandomas@gmail.com>
2018-09-08 23:59:23 +0200

    Move tasks data to data directory

--- client/src/pages/Home/Home.tsx
+++ client/src/pages/Home/Home.tsx
@@ -1,78 +1,7 @@
 import { createStyles, WithStyles, withStyles } from '@material-ui/core/styles'
 import React from 'react'
+import { tasks } from '~data'
 import { TaskList } from '~generic'
-
-const now = new Date().getTime()
-
-const tasks = [{
-  category: 'Matematyka',
-  expiredAt: now + 1000 * 60 * 100,
-  price: 100,
-  shortDescription: 'Rownanie różniczkowe analiza matematyczna',
-  tags: ['rownania', 'analiza']
-}, {
-  category: 'Fizyka',
-  expiredAt: now + 1000 * 60 * 10,
-  price: 100,
-  shortDescription: 'Masa marsa',
-  tags: ['astrologia']
-}, {
-  category: 'Muzyka',
-  expiredAt: now + 1000 * 60 * 10,
-  price: 140,
-  shortDescription: 'Odczytaj nuty i zagraj',
-  tags: ['nuty']
-}, {
-  category: 'Muzyka',
-  expiredAt: now + 1000 * 60 * 10,
-  price: 140,
-  shortDescription: 'Odczytaj nuty i zagraj',
-  tags: ['nuty']
-}, {
-  category: 'Muzyka',
-  expiredAt: now + 1000 * 60 * 10,
-  price: 140,
-  shortDescription: 'Odczytaj nuty i zagraj',
-  tags: ['nuty']
-}, {
-  category: 'Muzyka',
-  expiredAt: now + 1000 * 60 * 10,
-  price: 140,
-  shortDescription: 'Odczytaj nuty i zagraj',
-  tags: ['nuty']
-}, {
-  category: 'Muzyka',
-  expiredAt: now + 1000 * 60 * 10,
-  price: 140,
-  shortDescription: 'Odczytaj nuty i zagraj',
-  tags: ['nuty']
-}, {
-  category: 'Muzyka',
-  expiredAt: now + 1000 * 60 * 10,
-  price: 140,
-  shortDescription: 'Odczytaj nuty i zagraj',
-  tags: ['nuty']
-}, {
-  category: 'Muzyka',
-  expiredAt: now + 1000 * 60 * 10,
-  price: 140,
-  shortDescription: 'Odczytaj nuty i zagraj',
-  tags: ['nuty']
-}, {
-  category: 'Muzyka',
-  expiredAt: now + 1000 * 60 * 10,
-  price: 140,
-  shortDescription: 'Odczytaj nuty i zagraj',
-  tags: ['nuty']
-}]
-
-const styles = createStyles({
-  containerStyles: {
-    height: '100%'
-  }
-})
-
-interface HomeProps extends WithStyles<typeof styles> {}
 
 class Home extends React.Component <HomeProps> {
   public render () {
@@ -84,5 +13,13 @@ class Home extends React.Component <HomeProps> {
     )
   }
 }
+
+const styles = createStyles({
+  containerStyles: {
+    height: '100%'
+  }
+})
+
+interface HomeProps extends WithStyles<typeof styles> {}
 
 export default withStyles(styles)(Home)
