ebf1cf89b6e91a5a9efe6bd87996858c15268bf1
stpoa <stpoa@icloud.com>
2018-09-27 07:48:18 +0200

    Review fixes (3)

--- client/src/pages/Profile/components/ProfilePrivate.tsx
+++ client/src/pages/Profile/components/ProfilePrivate.tsx
@@ -3,38 +3,41 @@ import {
   ListItem, ListItemIcon, ListItemText,
   ListSubheader,
   StyleRulesCallback,
+  Typography,
   WithStyles,
   withStyles,
 } from '@material-ui/core'
-import { SvgIconProps } from '@material-ui/core/SvgIcon'
 import { AccountBalanceWallet, Inbox, Settings, Spellcheck, Update } from '@material-ui/icons'
-import React, { MouseEventHandler, SFC } from 'react'
-import { always } from '~lib/fp'
+import React, { SFC } from 'react'
 
-type ListItemData = [MouseEventHandler, React.ComponentType<SvgIconProps> | null, string | null, boolean]
-
-const handler = always(undefined) as MouseEventHandler
-const listData: ListItemData[] = [
-  [handler, null, null, false],
-  [handler, Update, 'Tasks in progress', true],
-  [handler, Inbox, 'Finished tasks', true],
-  [handler, Spellcheck, 'Reviews', true],
-  [handler, null, null, false],
-  [handler, Settings, 'Settings', true],
-]
 const ProfilePrivate: SFC<ProfilePrivatePropsStyled> = ({ balance, classes }) => (
   <div className={classes.root}>
-    <List subheader={<ListSubheader>Profile</ListSubheader>}>
+    <div className={classes.title}><Typography className={classes.titleText} variant="title">Profile</Typography></div>
+    <List subheader={<ListSubheader>Info</ListSubheader>}>
       <ListItem className={classes.balance}>
         <ListItemIcon><AccountBalanceWallet /></ListItemIcon>
         <ListItemText>Balance: {balance}</ListItemText>
       </ListItem>
-      {listData.map(([ handleClick, Icon, name, isButton ], i) => (
-        <ListItem key={i} button={isButton} onClick={handleClick}>
-          {Icon && <ListItemIcon><Icon/></ListItemIcon>}
-          {name && <ListItemText primary={name} />}
-        </ListItem>
-      ))}
+    </List>
+    <List subheader={<ListSubheader>Activity</ListSubheader>}>
+      <ListItem button>
+        <ListItemIcon><Update/></ListItemIcon>
+        <ListItemText primary="Tasks in progress" />
+      </ListItem>
+      <ListItem button>
+        <ListItemIcon><Inbox/></ListItemIcon>
+        <ListItemText primary={'Finished tasks'} />
+      </ListItem>
+      <ListItem button>
+        <ListItemIcon><Spellcheck/></ListItemIcon>
+        <ListItemText primary={'Reviews'} />
+      </ListItem>
+    </List>
+    <List subheader={<ListSubheader>Preferences</ListSubheader>}>
+      <ListItem button>
+        <ListItemIcon><Settings/></ListItemIcon>
+        <ListItemText primary="Settings" />
+      </ListItem>
     </List>
   </div>
 )
@@ -45,6 +48,12 @@ const styles: StyleRulesCallback = theme => ({
   },
   root: {
     margin: theme.spacing.unit,
+  },
+  title: {
+    margin: theme.spacing.unit * 2,
+  },
+  titleText: {
+    color: theme.palette.grey['700'],
   },
 })
 
