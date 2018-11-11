a085966e48aabe434362358396b872ad285f3b91
stpoa <stpoa@icloud.com>
2018-11-11 13:42:51 +0100

    Add button (#86)

---
+++ client/src/generic/Button.tsx
@@ -0,0 +1,31 @@
+import { Button as MaterialButton, StyleRulesCallback, withStyles,
+  WithStyles } from '@material-ui/core'
+import React, { MouseEventHandler, ReactNode, SFC } from 'react'
+
+const Button: SFC<ButtonProps> = ({ onClick, children, classes }: ButtonProps) => (
+  <MaterialButton
+    variant={'extendedFab'}
+    onClick={onClick}
+    className={classes.button}
+  >
+    {children}
+  </MaterialButton>
+)
+
+const styles: StyleRulesCallback = theme => ({
+  button: {
+    backgroundColor: theme.palette.secondary.main,
+    color: theme.palette.primary.main,
+    fontSize: '1.6rem',
+    height: '4rem',
+    minWidth: '6rem',
+    fontWeight: 600,
+  },
+})
+
+interface ButtonProps extends WithStyles<typeof styles> {
+  onClick?: MouseEventHandler<HTMLElement>
+  children: ReactNode
+}
+
+export default withStyles(styles)(Button)
