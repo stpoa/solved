f833e24c54df7071901a714d30fef0aeddfbc31e
Tomasz Klęk <tklek@icloud.com>
2019-05-22 15:04:29 +0200

    add margin in the end of tags list

--- client/src/pages/Search/SearchFilter.tsx
+++ client/src/pages/Search/SearchFilter.tsx
@@ -137,6 +137,7 @@ const styles: StyleRulesCallback = theme =>
     tags: {
       display: 'inline-flex',
       marginLeft: '0.5rem',
+      marginRight: '0.5rem',
       paddingTop: theme.spacing.unit,
       paddingBottom: theme.spacing.unit,
     },
