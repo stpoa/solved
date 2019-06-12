5af9a7ea196c3333b487b2db0712921b3a749743
stpoa <stpoaa+dev@gmail.com>
2019-06-12 16:04:23 +0200

    Add edit solution buttons

--- client/src/generic/Task/components/TaskForSolver.tsx
+++ client/src/generic/Task/components/TaskForSolver.tsx
@@ -1,9 +1,37 @@
+import { DoneAll as ConfirmIcon, Edit as EditIcon } from '@material-ui/icons'
 import React, { FC } from 'react'
+import Button from '~generic/Buttons/Button'
+import ButtonsContainer from '~generic/Buttons/ButtonsContainer'
 import { Task as TaskData } from '~interfaces'
+import { getTaskStatus, TaskStatus } from '../utils'
+import Solution from './Solution'
 import TaskBase from './TaskBase'
 
 const Task: FC<TaskProps> = ({ task }) => {
-  return <TaskBase {...{ task }} />
+  const editSolutionButtons = (
+    <ButtonsContainer>
+      <Button styleVariant="empty">
+        <EditIcon />
+        Edytuj
+      </Button>
+      <Button>
+        <ConfirmIcon />
+        Zatwierdź
+      </Button>
+    </ButtonsContainer>
+  )
+
+  const taskStatus = getTaskStatus(task)
+
+  const canEditSolution =
+    taskStatus === TaskStatus.Taken || taskStatus === TaskStatus.Started
+
+  return (
+    <TaskBase {...{ task }}>
+      {task.solution && <Solution solution={task.solution} />}
+      {canEditSolution && editSolutionButtons}
+    </TaskBase>
+  )
 }
 
 interface TaskProps {
